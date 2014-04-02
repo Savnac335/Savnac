254066618aa87f006fb81d184ab01ee5fa24d4af
joseph.davis <joseph.davis@mymail.champlain.edu>
2014-04-02 17:09:53 -0400

    added files for CourseRollModels and CourseRollController

---
+++ Savnac.Web/Controllers/CourseRollController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Savnac.Web.Controllers
+{
+    public class CourseRollController : Controller
+    {
+        //
+        // GET: /CourseRoll/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Savnac.Web/Models/CourseRollModels.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Globalization;
+using System.Web.Security;
+
+namespace Savnac.Web.Models
+{
+    
+    public class CourseRollContext : DbContext
+    {
+        public CourseRollContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<CourseRoll> CourseRolls { get; set; }
+    }
+
+    [Table("CourseRoll")]
+    public class CourseRoll
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int CourseRollId { get; set; }
+
+        /// This is where we will add the members to store data for a courseroll
+        /// ex. 
+        /// list<T> listofstudents
+        /// int CourseId
+        /// 
+    }
+    
+    
+    
+}
