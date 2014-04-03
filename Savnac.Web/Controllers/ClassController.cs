5fa5a5c631c268f330c8a797bd1614cdf08fefd9
Paul Kubik <paul.kubik@mymail.champlain.edu>
2014-04-03 06:11:44 -0400

    added class skeleton and ability to add classes

---
+++ Savnac.Web/Controllers/ClassController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Savnac.Web.Models;
+
+namespace Savnac.Web.Controllers
+{
+    public class ClassController : Controller
+    {
+ 
+        [AllowAnonymous]
+        public ActionResult SearchForClass()
+        {
+            
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public ViewResult addClassForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult addClassForm(Class classAdded)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("classAdded", classAdded);
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Savnac.Web/Models/ClassModels.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Globalization;
+using System.Web.Security;
+
+namespace Savnac.Web.Models
+{
+
+    [Table("Class")]
+    public class Class
+    {
+        [Required(ErrorMessage = "Please enter the class name") ]
+        public string ClassName { get; set; }
+        [Required(ErrorMessage = "Please enter the teacher's name")]
+        public string TeacherName { get; set; }
+
+    }
+}
