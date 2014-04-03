ecc8a0d5e9dc007b6f04bd08df4227c07464ecb3
zigmyalwangchuk <zigmyal.wangchuk@mymail.champlain.edu>
2014-04-03 18:50:42 -0400

    Forum initial commit

---
+++ Savnac.Web/Controllers/ThreadController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Transactions;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using DotNetOpenAuth.AspNet;
+using Microsoft.Web.WebPages.OAuth;
+using WebMatrix.WebData;
+using Savnac.Web.Filters;
+using Savnac.Web.Models;
+
+namespace Savnac.Web.Controllers
+{
+    public class ThreadController : Controller
+    {
+        //
+        // GET: /Thread/
+
+        public class ThreadDetialViewModel
+        {
+            public Thread Thread { get; set; }
+            public Post NewPost { get; set; }
+        }
+
+        public ActionResult ViewThreadDetail(int id)
+        {
+            var thread = new Thread() { ThreadID = id, ThreadTitle = "Savnac", Posts = new List<Post>() };
+            var newPost = new Post() { PostTitle = "", PostText = "", ThreadID = id };
+
+            return View(new ThreadDetialViewModel() { Thread = thread, NewPost = newPost });
+        }
+
+    }
+}
