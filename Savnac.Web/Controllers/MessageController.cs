e787d9e25403877752e3744b27cd09725550973a
fuzzyhunter0608 <fuzzyhunter0608@gmail.com>
2014-04-07 21:39:56 -0400

    Added Messages models, views, and controllers. Needs database implementation

---
+++ Savnac.Web/Controllers/MessageController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Savnac.Web.Models;
+
+namespace Savnac.Web.Controllers
+{
+    public class MessageController : Controller
+    {
+        //
+        // GET: /Message/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+		//
+		// GET: /Message/Inbox/
+
+		[Authorize]
+		public ActionResult Inbox()
+		{
+			MessageList messages = new MessageList();
+
+			return View(messages);
+		}
+
+		[Authorize]
+		public ActionResult ReadMessage(MessageModel model)
+		{
+			return View(model);
+		}
+    }
+}
---
+++ Savnac.Web/Models/MessageModels.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Savnac.Web.Models
+{
+	public class MessageModel
+	{
+		public string sender { get; set; }
+		public string receiver { get; set; }
+
+		public string subject { get; set; }
+		public string message { get; set; }
+
+		[DataType(DataType.DateTime)]
+		public DateTime timeSent { get; set; }
+
+		public bool isRead { get; set; }
+
+		public MessageModel()
+		{
+			isRead = false;
+			timeSent = DateTime.Now;
+		}
+	}
+
+	public class MessageList
+	{
+		public List<MessageModel> Messages { get; set; }
+
+		public MessageList()
+		{
+			Messages = new List<MessageModel>()
+			{
+				new MessageModel() {sender = "eric.pacelli", receiver = "james.peck", subject = "hi", message = "bye james"},
+				new MessageModel() {sender = "james.peck", receiver = "eric.pacelli", subject = "bye", message = "you suck"}
+			};
+		}
+	}
+}
