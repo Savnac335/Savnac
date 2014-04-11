54a89060136f40786d1269e05db7fb097a1f4c85
fuzzyhunter0608 <fuzzyhunter0608@gmail.com>
2014-04-10 21:38:04 -0400

    Added database connection to message system
    We've got 99 problems but a database ain't one.

--- Savnac.Database/messageTable.sql
+++ Savnac.Database/messageTable.sql
@@ -1,10 +1,10 @@
-﻿CREATE TABLE [dbo].[Table1]
+﻿CREATE TABLE [dbo].[messageTable]
 (
-	[msg_id] INT NOT NULL PRIMARY KEY, 
+	[msg_id] INT IDENTITY(1,1) PRIMARY KEY, 
     [msg_sEmail] VARCHAR(30) NOT NULL, 
     [msg_rEmail] VARCHAR(30) NOT NULL, 
     [msg_subject] VARCHAR(30) NOT NULL, 
     [msg_content] VARCHAR(MAX) NOT NULL, 
     [msg_dateTime] DATETIME NOT NULL, 
-    [msg_isRead] CHAR(3) NOT NULL
+    [msg_isRead] BIT NOT NULL
 )
--- Savnac.Web/Controllers/MessageController.cs
+++ Savnac.Web/Controllers/MessageController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Savnac.Web.Models;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace Savnac.Web.Controllers
 {
@@ -23,7 +25,33 @@ namespace Savnac.Web.Controllers
 		[Authorize]
 		public ActionResult Inbox()
 		{
-            MessageList messages = new MessageList();
+            List<MessageModel> messages = new List<MessageModel>();
+
+			var sql = string.Format("SELECT * FROM messageTable WHERE msg_rEmail = '{0}' ORDER BY msg_dateTime DESC", User.Identity.Name);
+			var connectionString = "Server=(local);Database=Savnac.Database;Trusted_Connection=True;";
+
+			var command = new SqlCommand(sql, new SqlConnection(connectionString));
+
+			using (var connection = command.Connection)
+			{
+				connection.Open();
+
+				using (var reader = command.ExecuteReader(CommandBehavior.CloseConnection))
+				{
+					while (reader.Read())
+					{
+						messages.Add(new MessageModel()
+						{
+							sender = reader["msg_sEmail"].ToString(),
+							recipient = reader["msg_rEmail"].ToString(),
+							subject = reader["msg_subject"].ToString(),
+							message = reader["msg_content"].ToString(),
+							timeSent = (DateTime)reader["msg_dateTime"],
+							isRead = (bool)reader["msg_isRead"]
+						});
+					}
+				}
+			}
 
 			return View(messages);
 		}
@@ -43,18 +71,30 @@ namespace Savnac.Web.Controllers
         [HttpPost]
         public ActionResult ComposeMessage(MessageModel model)
         {
-            //if (ModelState.IsValid)
-            //{
-            //    System.Data.SqlClient.SqlConnection sqlConnection1 = new System.Data.SqlClient.SqlConnection("Data Source=..\\Savnac.Database\\MyDatabase#1.sdf;Password=***********;Persist Security Info=True");
-            //    System.Data.SqlClient.SqlCommand cmd = new System.Data.SqlClient.SqlCommand();
-            //    cmd.CommandType = System.Data.CommandType.Text;
-            //    cmd.CommandText = "INSERT INTO messageTable (msg_sEmail, msg_rEmail, msg_subject, msg_content, msg_dateTime, msg_isRead ) VALUES (" + model.sender + ", " + model.recipient + ", " + model.subject + ", " + model.message + ", " + model.timeSent + ", " + model.isRead +")";
-            //    cmd.Connection = sqlConnection1;
+			if (ModelState.IsValid)
+			{
+				var sql = string.Format("INSERT INTO messageTable (msg_sEmail, msg_rEmail, msg_subject, msg_content, msg_dateTime, msg_isRead) VALUES ('{0}', '{1}', '{2}', '{3}', '{4}', '{5}')", User.Identity.Name, model.recipient, model.subject, model.message, DateTime.Now.ToString("yyyy-MM-dd HH:MM:ss"), false);
+				var connectionString = "Server=(local);Database=Savnac.Database;Trusted_Connection=True;";
 
-            //    sqlConnection1.Open();
-            //    cmd.ExecuteNonQuery();
-            //    sqlConnection1.Close();
-            //}
+				var command = new SqlCommand(sql, new SqlConnection(connectionString));
+
+				using (var connection = command.Connection)
+				{
+					connection.Open();
+					command.ExecuteNonQuery();
+					connection.Close();
+				}
+
+				/*System.Data.SqlClient.SqlConnection sqlConnection1 = new System.Data.SqlClient.SqlConnection("Data Source=..\\Savnac.Database\\MyDatabase#1.sdf;Password=***********;Persist Security Info=True");
+				System.Data.SqlClient.SqlCommand cmd = new System.Data.SqlClient.SqlCommand();
+				cmd.CommandType = System.Data.CommandType.Text;
+				cmd.CommandText = "INSERT INTO messageTable (msg_sEmail, msg_rEmail, msg_subject, msg_content, msg_dateTime, msg_isRead ) VALUES (" + model.sender + ", " + model.recipient + ", " + model.subject + ", " + model.message + ", " + model.timeSent + ", " + model.isRead + ")";
+				cmd.Connection = sqlConnection1;
+
+				sqlConnection1.Open();
+				cmd.ExecuteNonQuery();
+				sqlConnection1.Close();*/
+			}
 
             return RedirectToAction("SendMessage");
         }
