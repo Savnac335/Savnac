8f5c6a7269d7d13f82a5df519123bc12a07602d9
kennedy <kennedy.tran@mymail.champlain.edu>
2014-04-17 17:31:21 -0400

    Added Attendance model.

---
+++ Savnac.Web/Controllers/AttendanceController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Savnac.Web.Models;
+
+namespace Savnac.Web.Controllers
+{
+    public class AttendanceController : Controller
+    {
+        //
+        // GET: /Attendance/
+
+        public ActionResult CourseAttendance()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult SaveAttendance()
+        {
+            return View("CourseAttendance");
+        }
+
+        [HttpPost]
+        public ViewResult SaveAttendance(CourseAttendanceModel model)
+        {
+            ViewBag.isPresent = false;
+
+            return View("AttendanceSaved", model);
+        }
+    }
+}
---
+++ Savnac.Web/Models/CourseAttendanceModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Savnac.Web.Models
+{
+    public class CourseAttendanceModel
+    {
+        public List<string> professorNameList { get; set; }
+        public List<string> studentNameList { get; set; }
+        public bool isPresent { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+    }
+
+    public class onSave
+    {
+        public void Save(CourseAttendanceModel model)
+        {
+            for (int i = 0; model.studentNameList.Count != 0; i++)
+            {
+                var query = string.Format("Insert Into attendanceTable (studentName, isPresent, currentDate) Values ({0}, {1}, {3})", model.studentNameList[i], model.isPresent);
+                var connectionString = "Server=(local);Database=Savnac.Database;Trusted_Connection=True;";
+
+                var command = new SqlCommand(query, new SqlConnection(connectionString));
+
+                using (var connection = command.Connection)
+                {
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
+        }
+    }
+}
