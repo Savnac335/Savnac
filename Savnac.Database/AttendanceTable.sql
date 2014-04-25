210fa0105ac2cad415005cd15f91bee04a47d508
kennedy <kennedy.tran@mymail.champlain.edu>
2014-04-25 09:50:30 -0400

    Edited Tables for Attendance

---
+++ Savnac.Database/AttendanceTable.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Attendance] (
+    [studentName]  NVARCHAR (100) NOT NULL,
+    [isPresent]    BIT            NOT NULL,
+    [currentDate]  DATE           NOT NULL,
+    PRIMARY KEY CLUSTERED ([studentName] ASC)
+);
